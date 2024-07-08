b608114fd15a68f8ac42de2e490485af62abee63
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-08 21:25:57 +0600

    second commit

---
+++ append.js
@@ -0,0 +1,11 @@
+
+// 1. where to add
+const placeList = document.getElementsByClassName('places')[0]
+// 2. what to be added
+const li = document.createElement('li')
+li.innerText = 'lorem arah harah arh ara oijg aknj'
+
+// 3. add the child
+placeList.appendChild(li)
+
+// console.log(place)
