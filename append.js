2c6577eb4b5b3c88717ee2a59b1ab6968814dcdb
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-08 23:14:36 +0600

    3rd commit

--- append.js
+++ append.js
@@ -9,3 +9,20 @@ li.innerText = 'lorem arah harah arh ara oijg aknj'
 placeList.appendChild(li)
 
 // console.log(place)
+
+// 1. where to add
+const mainContainer = document.getElementById('main-container')
+const section = document.createElement('section')
+const h1 = document.createElement('h1')
+h1.innerText = 'this is a header'
+section.appendChild(h1)
+
+const ul = document.createElement('ul')
+const li1 = document.createElement('li')
+li1.innerText = 'llalalal laaa'
+ul.appendChild(li1)
+
+section.appendChild(ul)
+
+mainContainer.appendChild(section)
+console.log(mainContainer)
