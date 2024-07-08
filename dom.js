b64411cebe14a7d0ff42020897f8f94f68d908e0
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-08 18:04:49 +0600

    7th commit

--- dom.js
+++ dom.js
@@ -1,3 +1,10 @@
 
-console.log('hello from js')
-console.log(document.body)
+const sections = document.querySelectorAll('section')
+for(const section of sections){
+   section.style.border = '2px solid steelblue'
+}
+
+const places = document.getElementsByClassName('places')
+for(const place of places){
+    place.style.backgroundColor = 'lightgray'
+}
